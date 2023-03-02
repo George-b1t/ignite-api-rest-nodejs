32d63b9a3814310b7e893dc73dc468901491f3c7
George Soares <george.soares509@gmail.com>
2023-03-02 16:40:29 -0300

    Criação de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,26 @@
 import { FastifyInstance } from 'fastify'
+import z from 'zod'
 import { knex } from '../database'
+import crypto from 'node:crypto'
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions')
-      .where('amount', 1000)
-      .select('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const body = createTransactionBodySchema.parse(request.body)
+
+    const { title, amount, type } = body
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    reply.status(201).send()
   })
 }
